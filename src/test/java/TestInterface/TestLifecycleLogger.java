4695c460a50e60c9c5e64da8bf0beb1937d17bbf
Vedant <vedantrahane01@gmail.com>
2019-09-24 16:14:46 -0400

    Test interface demo

---
+++ src/test/java/TestInterface/TestLifecycleLogger.java
@@ -0,0 +1,38 @@
+package TestInterface;
+
+import java.util.logging.Logger;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.TestInfo;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+
+@TestInstance(Lifecycle.PER_CLASS)
+public interface TestLifecycleLogger {
+
+  static final Logger logger = Logger.getLogger(TestLifecycleLogger.class.getName());
+
+  @BeforeAll
+  default void beforeAllTests() {
+    logger.info("Before all tests");
+  }
+
+  @AfterAll
+  default void afterAllTests() {
+    logger.info("After all tests");
+  }
+
+  @BeforeEach
+  default void beforeEachTest(TestInfo testInfo) {
+    logger.info(() -> String.format("About to execute[%s]", testInfo.getDisplayName()));
+  }
+
+  @AfterEach
+  default void afterEachTest(TestInfo testInfo) {
+    logger.info(() -> String.format("Finished executing [%s]"));
+    testInfo.getDisplayName();
+  }
+
+}
